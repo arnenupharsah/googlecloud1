0934c814704ee63712d41d52034189804ea89f34
arnenupharsah <arnenupharsah@gmail.com>
2019-06-29 09:40:52 +0530

    1st time

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+
+//Getting user uploaded file
+
+$file = $_FILES["file"];
+//Saving file in uploads folder
+
+move_uploaded_file($file["tmp_name"],"../uploads/".$file["name"]);
+move_uploaded_file($file["tmp_name"],"uploads/".$file["name"]);
+
+// Redirecting back to home
+
+header("Location: smallobj.php");
+
+?>
